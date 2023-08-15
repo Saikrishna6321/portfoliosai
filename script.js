270a7f49b5a55ca3637c4360a2bd3b16620511ad
Saikrishna6321 <142136096+Saikrishna6321@users.noreply.github.com>
2023-08-15 22:51:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let menuIcon = document.querySelector('#menu-icon');
-let navbar =document.querySelector('.nav-bar');
-menuIcon.oneclick =() =>{
-    menuIcon.classList.toggle('fa-xmark');
+let navbar =document.querySelector('.nav-elements');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
 
@@ -9,7 +10,7 @@ menuIcon.oneclick =() =>{
 let sections =document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = ()=>{
+window.onscroll = () => {
     sections.forEach(sec=>{
         let top =window.scrollY;
         let offset = sec.offsetTop-150;
@@ -17,13 +18,36 @@ window.onscroll = ()=>{
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height  ){
-                    navLinks.forEach(links =>{
-                        links.classList.remove('active');
-                        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-                    });
+             navLinks.forEach(links => {
+                 links.classList.remove('active');
+                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
         };
     });
-    let header =documenet.querySelector('header');
+    let header =document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+ScrollReveal({
+     reset: true,
+     distance: '80px',
+     duration:2000,
+     delay:200
+ });
+
+ScrollReveal().reveal('.text , .heading', { origin: 'top' });
+ScrollReveal().reveal('.profile , .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.text h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.text p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.mutli',{
+     strings:['frontend developer','youtuber','blogger'],
+     typeSpeed: 100,
+     backSpeed: 50,
+     backDelay: 1000,
+     loop:true
+});
 
